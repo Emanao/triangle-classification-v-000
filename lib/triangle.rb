3029b255a2f4d896d4f95fd96b29ccf9b85496ee
Eva Navarro <emanao@me.com>
2019-11-18 20:04:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,7 @@ class Triangle
    kind=nil
    if (side_a<=0 || side_b<=0 || side_c<=0) ||
      (side_a+side_b<=side_c || side_a+side_c<=side_b || side_b+side_c<=side_a)
+     binding.pry
      begin
        raise TriangleError
      rescue TriangleError=> error
